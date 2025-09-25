c7cf10fa29da8e413bb8872ef7fcb857a95e8651
Samia Zaman <samiazaman174@gmail.com>
2025-09-26 01:10:16 +0600

    feat(admin): register models with image preview

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','role','university_id','photo_preview')
+    readonly_fields = ('photo_preview',)
+
+    def photo_preview(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" width="100" height="100"/>', obj.photo.url)
+        return "(No photo)"
+
+admin.site.register(User, UserAdmin)
--- transport/admin.py
+++ transport/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Route, Bus, Registration
+
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('number','route','capacity','time_slot','driver_preview')
+    readonly_fields = ('driver_preview',)
+
+    def driver_preview(self, obj):
+        if obj.driver_photo:
+            return format_html('<img src="{}" width="100" height="100"/>', obj.driver_photo.url)
+        return "(No photo)"
+
+admin.site.register(Route)
+admin.site.register(Bus, BusAdmin)
+admin.site.register(Registration)
