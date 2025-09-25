b1b40f590e36e3131b80cff61d2d26edc256cc4e
Samia Zaman <samiazaman174@gmail.com>
2025-09-26 01:05:15 +0600

    chore: run initial migrations

---
+++ uap_tms/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for uap_tms project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uap_tms.settings')
+
+application = get_asgi_application()
