33d8ddb1ef298c7497731eaaab404ed63333448a
Samia Zaman <samiazaman174@gmail.com>
2025-09-26 01:21:17 +0600

    Finalized Django Transport Management System with custom User, transport, payments, tracking

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from django.utils.html import format_html
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username','role','university_id','photo_preview')
-    readonly_fields = ('photo_preview',)
-
-    def photo_preview(self, obj):
-        if obj.photo:
-            return format_html('<img src="{}" width="100" height="100"/>', obj.photo.url)
-        return "(No photo)"
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'email', 'role', 'university_id', 'is_staff')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email', 'university_id')
+    ordering = ('id',)
 
 admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # You can add extra fields if needed, for now keep it simple
-    phone = models.CharField(max_length=15, blank=True, null=True)
+    university_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
+
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('staff', 'Staff'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
 
     def __str__(self):
         return self.username
